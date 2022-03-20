11665e5c3720a56228c5a57979a9e937d5ee8ba6
kyungmin <nkm4095@naver.com>
2022-03-20 20:53:48 +0900

    spring study

--- src/main/java/com/fastcampus/ch3/Controller_RequestMapping.java
+++ src/main/java/com/fastcampus/ch3/Controller_RequestMapping.java
@@ -1,17 +1,20 @@
 package com.fastcampus.ch3;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @Controller
+@RequestMapping("/common")
 public class Controller_RequestMapping {
 
-    @RequestMapping("/first")
-    public void requestmapping(HttpServletResponse httpServletResponse) throws IOException {
+    @RequestMapping("/requestmapping")
+    public void requestmapping_get(HttpServletResponse httpServletResponse) throws IOException {
         System.out.println("hello spring :" +"hello spring");
 
         httpServletResponse.setContentType("text/html");
@@ -25,14 +28,30 @@ public class Controller_RequestMapping {
         printWriter.println("</title>");
         printWriter.println("</head>");
         printWriter.println("<body>");
+        printWriter.println("@Requestmapping('/first')");
+        printWriter.println("Only Get request");
+
+
         printWriter.println("<p style='color:pink; font-size: 30px'>first Srping </p>");
         printWriter.println("</body>");
         printWriter.println("</html>");
     }
-    @RequestMapping("/second")
-    public String requestmappingview(){
-
-        return "firstspring";
+    @RequestMapping(value = "/requestmapping_views_false",method=RequestMethod.GET)
+    public String requestmapping_views(@RequestParam(required = false) int year,
+                                       @RequestParam(name = "month",required = false) int month,
+                                       int day, Model model){
+        model.addAttribute("year",year);
+        model.addAttribute("month",month);
+        model.addAttribute("day",day);
+        return "getrequest_Int";
+    }    @RequestMapping(value = "/requestmapping_views_requiered_true",method=RequestMethod.GET)
+    public String requestmapping_views_param(@RequestParam int year,
+                                             @RequestParam(required = true) int month,
+                                             @RequestParam(name = "day",required = true) int day, Model model){
+        model.addAttribute("year",year);
+        model.addAttribute("month",month);
+        model.addAttribute("day",day);
+        return "getrequest_Int";
     }
 
 }
