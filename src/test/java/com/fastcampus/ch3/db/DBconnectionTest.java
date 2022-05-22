520ed46562f52a0c310ad4b5c60b5fa5c107d3c2
kyungmin <nkm4095@naver.com>
2022-05-22 19:44:29 +0900

    spring study

--- src/test/java/com/fastcampus/ch3/db/DBconnectionTest.java
+++ src/test/java/com/fastcampus/ch3/db/DBconnectionTest.java
@@ -1,5 +1,7 @@
 package com.fastcampus.ch3.db;
 
+import com.mysql.cj.protocol.Resultset;
+import org.apache.ibatis.jdbc.SQL;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.sql.DataSource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -37,10 +43,81 @@ public class DBconnectionTest {
     //bean 주입받아서 자동으로 DB접근 테스트
     @Autowired
     DataSource ds;
+
+
     @Test
     public void mybatis_dbconnection_test() throws SQLException{
         Connection conn= ds.getConnection(); // SQLException을 발생시킴
         System.out.println("conn = " + conn);
         assertTrue(conn!=null);
     }
+
+    @Test
+    public void db_insert_test() throws SQLException{
+        String sql="INSERT INTO db_table VALUES(?,?)";
+        try(Connection conn=ds.getConnection();
+            PreparedStatement pstmt=conn.prepareStatement(sql)
+
+                ){
+
+                pstmt.setString(1, "아이디임");
+                pstmt.setString(2, "비밀번호임");
+                pstmt.executeUpdate();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void db_delete_tset() throws SQLException{
+        String sql="delete FROM db_table WHERE id=?";
+        try(Connection conn=ds.getConnection();
+            PreparedStatement pstmt=conn.prepareStatement(sql)
+
+        ){
+            pstmt.setString(1,"아이디임10");
+            pstmt.executeUpdate();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void db_update_test() throws SQLException {
+        String sql="UPDATE db_table SET pw=? WHERE id=?";
+        try(Connection conn=ds.getConnection();
+            PreparedStatement pstmt=conn.prepareStatement(sql)
+
+        ){
+            pstmt.setString(1,"비밀번호임1");
+            pstmt.setString(2,"아이디임");
+            pstmt.executeUpdate();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void db_select_test() throws SQLException{
+        String sql="SELECT * FROM db_table";
+        try(Connection conn=ds.getConnection();
+            PreparedStatement pstmt=conn.prepareStatement(sql);
+            ResultSet rs=pstmt.executeQuery()
+
+        ){
+
+            while(rs.next()){
+                System.out.println( "id = "+rs.getString("id")+" "+
+                        "pw ="+rs.getString("pw"));
+                System.out.println("---------");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
